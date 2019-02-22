0917105d6edcc9ce38c19403bc216850f6a455d4
Justin Barrick <justin.m.barrick@gmail.com>
2019-02-21 19:06:17 -0800

    implement backup controller

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"sync"
+	"fmt"
+	"time"
 	"context"
 	corev1 "k8s.io/api/core/v1"
+	snapshots "github.com/kubernetes-csi/external-snapshotter/pkg/apis/volumesnapshot/v1alpha1"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"log"
 	"net/http"
+	//"github.com/davecgh/go-spew/spew"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/builder"
@@ -21,22 +33,172 @@ const (
 	webhookName      = "snapshot-webhook"
 )
 
-type latestSnapshotMutator struct {
+// Create a new VolumeSnapshot for a PVC.
+func takeBackup(backup BackupSchedule) func() {
+	return func() {
+		log.Printf("It is time for a backup of %s!", backup.name)
+	}
+}
+
+// Represents the backup schedule for a single PVC and invokes a timer accordingly.
+type BackupSchedule struct {
+	name string
+	volumeCreated time.Time
+	latest time.Time
+	interval *time.Duration
+	timer *time.Timer
+}
+
+// Schedule a backup for a PVC.
+func (b *BackupSchedule) Schedule() {
+	if b.interval == nil {
+		return
+	}
+
+	if b.timer != nil {
+		b.timer.Stop()
+	}
+
+	checkTime := b.volumeCreated
+
+	if b.latest != (time.Time{}) {
+		checkTime = b.latest
+	}
+
+	nextBackup := checkTime.Add(*b.interval).Sub(time.Now())
+
+	b.timer = time.AfterFunc(nextBackup, takeBackup(*b))
+
+	log.Printf("Backup for %s scheduled for %s", b.name, nextBackup)
+}
+
+// If t is more recent than the current latest, set latest to t.
+func (b *BackupSchedule) SetLatest(t time.Time) {
+	if b.latest == (time.Time{}) {
+		b.latest = t
+	}
+
+	if t.Sub(b.latest).Seconds() > 0 {
+		b.latest = t
+	}
+}
+
+// Set the interval and volumeCreated settings from the PVC.
+func (b *BackupSchedule) SetInterval(pvc *corev1.PersistentVolumeClaim) error {
+	b.volumeCreated = pvc.ObjectMeta.CreationTimestamp.Time
+
+	annotations := pvc.GetAnnotations()
+	if annotations == nil || annotations["snapshot-frequency"] == "" {
+		return nil
+	}
+
+	snapshotFrequency, err := time.ParseDuration(annotations["snapshot-frequency"])
+	if err != nil {
+		return err
+	}
+
+	b.interval = &snapshotFrequency
+	return nil
+}
+
+// A map and lock for controlling access to BackupSchedules.
+type Backups struct {
+	backups map[string]*BackupSchedule
+	lock sync.Mutex
+}
+
+// Retrieve a BackupSchedule by key. If it does not exist, it will be initialized.
+func (b *Backups) Get(key string) *BackupSchedule {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	if b.backups == nil {
+		b.backups = map[string]*BackupSchedule{}
+	}
+
+	if b.backups[key] == nil {
+		b.backups[key] = &BackupSchedule{name: key}
+	}
+
+	return b.backups[key]
+}
+
+// Reconciler for reacting to VolumeSnapshot events.
+type ReconcileVolumeSnapshots struct {
+	client client.Client
+	backups Backups
+}
+
+// Reconcile VolumeSnapshot events by updating the backups map with known snapshots.
+func (r *ReconcileVolumeSnapshots) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	log.Println("Reconciling snapshot:", request.NamespacedName)
+	snapshot := &snapshots.VolumeSnapshot{}
+
+	err := r.client.Get(context.TODO(), request.NamespacedName, snapshot)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Println("Snapshot not found:", request.NamespacedName)
+			return reconcile.Result{}, nil
+		}
+
+		return reconcile.Result{}, err
+	}
+
+	backup := r.backups.Get(fmt.Sprintf("%s/%s", snapshot.GetNamespace(), snapshot.Spec.Source.Name))
+	backup.SetLatest(snapshot.ObjectMeta.CreationTimestamp.Time)
+	backup.Schedule()
+
+	return reconcile.Result{}, nil
+}
+
+// Reconciler for reacting to PersistentVolumeClaim events.
+type ReconcilePersistentVolumeClaims struct {
+	client client.Client
+	backups Backups
+}
+
+// Reconcile PersistentVolumeClaims by updating the backups map with information about the PVC.
+func (r *ReconcilePersistentVolumeClaims) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	log.Println("Reconciling PVC:", request.NamespacedName)
+	pvc := &corev1.PersistentVolumeClaim{}
+
+	err := r.client.Get(context.TODO(), request.NamespacedName, pvc)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Println("PVC not found:", request.NamespacedName)
+			return reconcile.Result{}, nil
+		}
+
+		return reconcile.Result{}, err
+	}
+
+	backup := r.backups.Get(request.NamespacedName.String())
+	backup.SetInterval(pvc)
+	backup.Schedule()
+
+	return reconcile.Result{}, nil
+}
+
+// Kubernetes validating webhook for updating PersistentVolumeClaims with a reference to the most recent backup taken.
+type LatestSnapshotMutator struct {
 	client  client.Client
 	decoder types.Decoder
 }
 
-func (v *latestSnapshotMutator) InjectClient(c client.Client) error {
+// Implement InjectClient
+func (v *LatestSnapshotMutator) InjectClient(c client.Client) error {
 	v.client = c
 	return nil
 }
 
-func (v *latestSnapshotMutator) InjectDecoder(d types.Decoder) error {
+// Implement InjectDecoder
+func (v *LatestSnapshotMutator) InjectDecoder(d types.Decoder) error {
 	v.decoder = d
 	return nil
 }
 
-func (v *latestSnapshotMutator) mutatePvc(pvc *corev1.PersistentVolumeClaim) {
+// Set the DataSource on a persistent volume claim if the latest-snapshot key is present.
+func (v *LatestSnapshotMutator) MutatePvc(pvc *corev1.PersistentVolumeClaim) {
 	annotations := pvc.ObjectMeta.GetAnnotations()
 	if annotations == nil || annotations[annotationKey] == "" {
 		return
@@ -51,7 +213,8 @@ func (v *latestSnapshotMutator) mutatePvc(pvc *corev1.PersistentVolumeClaim) {
 	}
 }
 
-func (v *latestSnapshotMutator) Handle(ctx context.Context, req types.Request) types.Response {
+// Handle an incoming webhook.
+func (v *LatestSnapshotMutator) Handle(ctx context.Context, req types.Request) types.Response {
 	pvc := &corev1.PersistentVolumeClaim{}
 
 	err := v.decoder.Decode(req, pvc)
@@ -60,15 +223,46 @@ func (v *latestSnapshotMutator) Handle(ctx context.Context, req types.Request) t
 	}
 
 	newPvc := pvc.DeepCopy()
-	v.mutatePvc(newPvc)
+	v.MutatePvc(newPvc)
 
 	return admission.PatchResponse(pvc, newPvc)
 }
 
 func main() {
-	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	scheme := runtime.NewScheme()
+	snapshots.AddToScheme(scheme)
+	corev1.AddToScheme(scheme)
+	admissionregistrationv1beta1.AddToScheme(scheme)
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
+		Scheme: scheme,
+	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("cannot create manager:", err)
+	}
+
+	backups := Backups{}
+
+	snapshotController, err := controller.New("snapshot-controller", mgr, controller.Options{
+		Reconciler: &ReconcileVolumeSnapshots{client: mgr.GetClient(), backups: backups},
+	})
+	if err != nil {
+		log.Fatal("cannot create snapshot controller:", err)
+	}
+
+	if err := snapshotController.Watch(&source.Kind{Type: &snapshots.VolumeSnapshot{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		log.Fatal("cannot watch VolumeSnapshots:", err)
+	}
+
+	pvcController, err := controller.New("pvc-controller", mgr, controller.Options{
+		Reconciler: &ReconcilePersistentVolumeClaims{client: mgr.GetClient(), backups: backups},
+	})
+	if err != nil {
+		log.Fatal("cannot create PVC controller:", err)
+	}
+
+	if err := pvcController.Watch(&source.Kind{Type: &corev1.PersistentVolumeClaim{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		log.Fatal("cannot watch PVCs:", err)
 	}
 
 	mutatingWebhook, err := builder.NewWebhookBuilder().
@@ -76,10 +270,10 @@ func main() {
 		WithManager(mgr).
 		Path("/mutate").
 		ForType(&corev1.PersistentVolumeClaim{}).
-		Handlers(&latestSnapshotMutator{}).
+		Handlers(&LatestSnapshotMutator{}).
 		Build()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("cannot create webhook:", err)
 	}
 
 	as, err := webhook.NewServer(webhookName, mgr, webhook.ServerOptions{
@@ -87,16 +281,14 @@ func main() {
 		CertDir: "/tmp/cert",
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("cannot start webhook server:", err)
 	}
 
-	err = as.Register(mutatingWebhook)
-	if err != nil {
-		log.Fatal(err)
+	if err := as.Register(mutatingWebhook); err != nil {
+		log.Fatal("cannot register webhook", err)
 	}
 
-	err = mgr.Start(signals.SetupSignalHandler())
-	if err != nil {
-		log.Fatal(err)
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Fatal("cannot start manager:", err)
 	}
 }
